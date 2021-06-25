4ed70320f7ddae28ccbb09e5f317bda115517d59
Giovanna Severo <giovanna-severo@hotmail.com>
2021-06-24 22:12:22 -0300

    configuração NotFoundException

--- controleveiculos/src/main/java/com/oragetalents/controleveiculos/exception/RegistroNaoEncontradoException.java
+++ controleveiculos/src/main/java/com/oragetalents/controleveiculos/exception/RegistroNaoEncontradoException.java
@@ -1,5 +1,9 @@
 package com.oragetalents.controleveiculos.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RegistroNaoEncontradoException extends RuntimeException {
 	public RegistroNaoEncontradoException(String msg) {
 		super(msg);
